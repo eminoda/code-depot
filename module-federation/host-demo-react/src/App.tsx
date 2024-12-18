8b6ed80a9df6c8b7440f4efb567213ef33928950
shixinghao <245978782@qq.com>
2024-12-18 23:12:59 +0800

    chore

--- module-federation/host-demo-react/src/App.tsx
+++ module-federation/host-demo-react/src/App.tsx
@@ -83,10 +83,117 @@ const App = () => {
       <p>remote-react-rsbuild</p>
       <RemoteReactRsbuildButton type="primary" name="弹框1" />
       <div style={{ width: "600px" }}>
-        <RemoteReactRsbuildForm />
+        <RemoteReactRsbuildForm
+          {...{
+            title: "用户注册",
+            api: "/amis/api/mock2/form/saveForm",
+            fields: [
+              {
+                type: "input-text",
+                name: "name",
+                label: "姓名：",
+              },
+              {
+                name: "email",
+                type: "input-email",
+                label: "邮箱：",
+              },
+            ],
+          }}
+        />
       </div>
       <div style={{ width: "1000px" }}>
-        <RemoteReactRsbuildTable />
+        <RemoteReactRsbuildTable
+          {...{
+            api: "/amis/api/mock2/sample",
+            columns: [
+              {
+                name: "id",
+                label: "ID",
+              },
+              {
+                name: "engine",
+                label: "Rendering engine",
+              },
+              {
+                name: "browser",
+                label: "Browser",
+              },
+              {
+                name: "platform",
+                label: "Platform(s)",
+              },
+              {
+                name: "version",
+                label: "Engine version",
+              },
+              {
+                name: "grade",
+                label: "CSS grade",
+              },
+              {
+                type: "operation",
+                label: "操作",
+                buttons: [
+                  {
+                    label: "详情",
+                    type: "button",
+                    level: "link",
+                    actionType: "dialog",
+                    dialog: {
+                      title: "查看详情",
+                      body: {
+                        type: "form",
+                        api: "post:/amis/api/mock2/sample/${id}",
+                        body: [
+                          {
+                            type: "input-text",
+                            name: "engine",
+                            label: "Engine",
+                          },
+                          {
+                            type: "input-text",
+                            name: "browser",
+                            label: "Browser",
+                          },
+                          {
+                            type: "input-text",
+                            name: "platform",
+                            label: "platform",
+                          },
+                          {
+                            type: "input-text",
+                            name: "version",
+                            label: "version",
+                          },
+                          {
+                            type: "control",
+                            label: "grade",
+                            body: {
+                              type: "tag",
+                              label: "${grade}",
+                              displayMode: "normal",
+                              color: "active",
+                            },
+                          },
+                        ],
+                      },
+                    },
+                  },
+                  {
+                    label: "删除",
+                    type: "button",
+                    level: "danger",
+                    disabledOn: "this.grade === 'A'",
+                    confirmText: "确认要删除？",
+                    api: "delete:/amis/api/mock2/sample/${id}",
+                    actionType: "ajax",
+                  },
+                ],
+              },
+            ],
+          }}
+        />
       </div>
       {/* <p>remote-react-vite</p>
       <div>
