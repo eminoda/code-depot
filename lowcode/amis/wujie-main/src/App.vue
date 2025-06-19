d39d887f65521775d0b3a09e7dc0a4f670d55d8a
shixinghao <shixinghao@sheca.com>
2025-06-19 13:59:51 +0800

    chore: 测试amis wujie 集成

--- lowcode/amis/wujie-main/src/App.vue
+++ lowcode/amis/wujie-main/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1>我是主项目</h1>
+    <!-- <h1>我是主项目</h1> -->
     <div id="wujie-container"></div>
   </div>
 </template>
@@ -11,10 +11,29 @@ import { startApp } from "wujie";
 export default {
   name: "App",
   mounted() {
+    console.log(123)
     startApp({
       name: "wujie-amis",
-      url: "http://localhost:8081",
+      // url: "http://localhost:8081",
+      url: "http://127.0.0.1:8888/examples/crud/table?orderBy=id&orderDir=asc&page=1",
       el: document.getElementById("wujie-container"),
+      // 为所有资源请求（包括CSS）设置请求头
+      // plugins: {
+      //   cssLoader: (code, url, base) => {
+      //     console.log(code, url,base)
+      //     return url
+      //   }
+      // },
+      fetch: (url) => {
+        console.log(url)
+        if (url.indexOf('.css')) {
+          return fetch(url, {
+            headers: {
+              'Accept': 'text/css,*/*;q=0.1'
+          }})
+        }
+        return fetch(url)
+      }
     });
   },
 };
--- lowcode/amis/wujie-main/vue.config.js
+++ lowcode/amis/wujie-main/vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      "/packages": {
+        target: 'http://127.0.0.1:8888',
+        origin: true
+      },
+      "/examples": {
+        target: 'http://127.0.0.1:8888',
+        origin: true
+      },
+    }
+  }
 })
